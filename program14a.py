0d80ac869f09a80722bf492b381bafac112c5679
charlene-khun <charlenek2009@gmail.com>
2024-01-21 02:07:14 -0800

    first commit.

---
+++ program14a.py
@@ -0,0 +1,20 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+dic1 = dict()
+for line in handle:
+    if line.startswith('From '):
+        words = line.split()
+        #only looking at the second word of each line, doesn't need another for loop
+        word1 = words[1]
+        dic1[word1] = dic1.get(word1,0) + 1
+ 
+max1 = None
+bigword = None
+for word,count in dic1.items():
+    if max1 is None or count > max1:
+        bigword = word
+        max1 = count
+        
+print(bigword,max1)
